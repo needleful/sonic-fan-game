d5f125d1cb0b6971dccbcde2fa279c58b5ba67e3
LeeHastings <lee.hastings@outlook.com>
2020-08-31 22:41:48 -0500

    Flying works great now

--- scripts/control/badnik_flying.gd
+++ scripts/control/badnik_flying.gd
@@ -5,50 +5,52 @@ export(NodePath) var weaponNode: NodePath = NodePath("Eye")
 
 export(float) var player_outer_radius = 8
 export(float) var player_inner_radius = 3
-export(float) var acceleration = 25
+export(float) var acceleration = 28
 export(float) var pivot_acceleration = 25
 export(float) var torque_to_player = 10
 export(float) var torque_to_reorient = 10
 export(float) var repulsion = 10
 export(float) var avoidance_strength = 18
 export(float) var angular_drag = .9
-export(float) var weapon_rotation_speed: float = deg2rad(33)
+export(float) var weapon_rotation_speed: float = deg2rad(40)
 
 const DRAG_AIR = 0.00005
 onready var sonic: Sonic = get_node(sonicNode)
 onready var weapon: Spatial = get_node(weaponNode)
 onready var anim: AnimationPlayer = $AttackAnimations
+onready var attackArea = $Eye/AttackMesh/AttackArea
 
-var aim = false
-var firing = false
-var commit = false
-var cancel = false
+enum WeaponState {
+	Idle
+	Charging,
+	Committed,
+	Firing,
+	Cancelling
+}
 
-const TIME_TO_CANCEL = .65
+var weapState = WeaponState.Idle
+
+const TIME_TO_CANCEL = .85
 var cancel_timer = 0
 
-func _ready():
-	var _c = $Eye/AttackArea.connect("body_entered", self, "onAttackEnter")
-	_c = $Eye/AttackArea.connect("body_exited", self, "onAttackExit")
-
 func _physics_process(delta):
-	if firing:
-		for b in $Eye/AttackArea.get_overlapping_bodies():
-			if b != self and b.has_method("die"):
-				b.die()
-	elif cancel:
-		cancel_timer += delta
-		if commit:
-			cancel = false
-			cancel_timer = 0
-		elif cancel_timer >= TIME_TO_CANCEL:
-			anim.play("CancelAttack")
-			aim = false
-			cancel = false
-			commit = false
-			cancel_timer = 0
+	match weapState:
+		WeaponState.Idle:
+			if attackArea.get_overlapping_bodies().has(sonic):
+				set_state(WeaponState.Charging)
+		WeaponState.Firing:
+			for b in attackArea.get_overlapping_bodies():
+				if b != self and b.has_method("die"):
+					b.die()
+		WeaponState.Charging:
+			if !anim.is_playing() or anim.current_animation != "Attack":
+				anim.play("Attack")
+			if !attackArea.get_overlapping_bodies().has(sonic):
+				cancel_timer += delta
+				if cancel_timer >= TIME_TO_CANCEL:
+					set_state(WeaponState.Cancelling)
 	
-	var dist = sonic.global_transform.origin + sonic.velocity*delta - global_transform.origin
+	var dist = sonic.attack_position() + sonic.velocity*delta - weapon.global_transform.origin
 	var dir:Vector3 = (dist).normalized()
 	var f = global_transform.basis.z
 	var l = global_transform.basis.x
@@ -90,7 +92,7 @@ func _physics_process(delta):
 	add_torque(rtorq)
 	add_torque(drtorq)
 	
-	var future_sonic = sonic.attack_position()
+	var future_sonic = sonic.attack_position() + sonic.velocity*delta
 	var weapdir = (future_sonic - weapon.global_transform.origin).normalized()
 	var angle = weapon.global_transform.basis.z.angle_to(weapdir)
 	var axis = weapon.global_transform.basis.z.cross(weapdir).normalized()
@@ -98,34 +100,28 @@ func _physics_process(delta):
 	if axis.length_squared() >= 0.999:
 		weapon.global_rotate(axis, sign(angle)*min(abs(angle), weapon_rotation_speed*delta))
 
-func kill_start():
-	firing = true
-
-func kill_end():
-	firing = false
-
-func onAttackEnter(body):
-	if !(body is Sonic) or body.dead:
+func set_state(state):
+	if weapState == state:
 		return
-	aim = true
-	cancel = false
 	cancel_timer = 0
-	if !anim.is_playing() and anim.current_animation != "Attack":
-		anim.play("Attack")
-
-func onAttackExit(body):
-	if body is Sonic:
-		if !commit:
-			cancel_timer = 0
-			cancel = true
-		else:
-			aim = false
+	weapState = state
+	match weapState:
+		WeaponState.Charging:
+			anim.play("Attack")
+		WeaponState.Cancelling:
+			anim.play("CancelAttack")
 
 func attack_commit():
-	commit = true
+	set_state(WeaponState.Committed)
 
-func attack_start():
-	commit = false
+func kill_start():
+	set_state(WeaponState.Firing)
+
+func kill_end():
+	set_state(WeaponState.Idle)
+
+func cancel_end():
+	set_state(WeaponState.Idle)
 
 func die():
 	queue_free()
