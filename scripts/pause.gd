dfd7ddcec6343f27720357eb9f8a134515fe4409
LeeHastings <lee.hastings@outlook.com>
2020-09-10 19:02:26 -0500

    Adding pause menu and options

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -1,14 +1,43 @@
-extends Node
+extends Control
 
 func _input(event):
 	if event.is_action_pressed("gm_pause"):
 		get_tree().paused = !get_tree().paused
-		if get_tree().paused:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		pause(get_tree().paused)
 	elif event.is_action_pressed("dev_reset"):
 		var _x = get_tree().reload_current_scene()
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pause(get_tree().paused)
+
+func pause(paused:bool):
+	visible = paused
+	if paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		show_main()
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_resume_pressed():
+	get_tree().paused = false
+	pause(false)
+
+func _on_Restart_pressed():
+	var _x = get_tree().reload_current_scene()
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+func show_main():
+	$MainMenu.visible = true
+	$FastOptionsMenu.visible = false
+
+func _on_Options_pressed():
+	$MainMenu.visible = false
+	$FastOptionsMenu.visible = true
+
+func _on_FastOptionsMenu_apply():
+	show_main()
+
+func _on_FastOptionsMenu_cancel():
+	show_main()
